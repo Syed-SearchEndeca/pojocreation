5500695e53a1ad24017a42efbe5edebed9d62e1c
Syed <Syedghouse14@hotmail.com>
2021-07-10 23:14:33 +0530

    Commited

---
+++ jaxb2MavenPojodemo/src/main/java/com/searchendeca/jaxbdemo/JaxbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.searchendeca.jaxbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxbdemoApplication.class, args);
+	}
+
+}
---
+++ jaxb2MavenPojodemo/src/test/java/com/searchendeca/jaxbdemo/JaxbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchendeca.jaxbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
